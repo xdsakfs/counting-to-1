59f13106a674e32f7fb384f8f0cd309cd7c5c624
AtreyuBriseno <100451379+AtreyuBriseno@users.noreply.github.com>
2023-06-13 20:41:07 -0700

    Update srcipts.js

--- srcipts.js
+++ srcipts.js
@@ -1,1 +1,11 @@
+let getData = fetch("time.json")
+let getDataWResponce = getData.then(response => response.json())
+let GDFJ = getDataWResponce.then(data => {let ctvar = data.time;})	//GDFJ means GatherdDataFromJson
 
+function main(){
+}
+function count(ct) {
+  ct = ct + 0.0000000000000001
+}
+function updateP(){
+}
